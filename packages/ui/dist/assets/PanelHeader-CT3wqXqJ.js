64c9ea700385756f279c8208a2c3558ff72a3ecc
Mohammad Obeid <mohammadkadoumi77@yahoo.com>
2026-08-08 22:05:38 +0300

    Adding dist

---
+++ packages/ui/dist/assets/PanelHeader-CT3wqXqJ.js
@@ -0,0 +1,2 @@
+import{_ as l,j as c,c as n,lY as t}from"./index-CjRqMbsf.js";const d=s=>{var{className:e,children:a}=s,r=l(s,["className","children"]);return c.jsx("div",Object.assign({className:n(t.panelHeader,e)},r,{children:a}))};d.displayName="PanelHeader";export{d as P};
+//# sourceMappingURL=PanelHeader-CT3wqXqJ.js.map
